0ca16d5c54528f3df5aa3b2b37340d47e69b65c8
Unknown <14054505+piegamesde@users.noreply.github.com>
2017-12-02 15:47:16 +0100

    Item handling and interactions

---
+++ Project Flausch/Assets/Scripts/Inventory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory {
+	public Item<Object> leftHand = null;
+	public Item<Object> rightHand = null;
+}
