0b8d8048d8c27e576f24285f0a3a4e0a3c82a415
saibotk <saibotk@users.noreply.github.com>
2017-12-02 12:04:16 +0100

    Basic setup

---
+++ Project Flausch/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField]
+	private int movementSpeed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("d")) {
+			gameObject.transform.Translate( new Vector3(1* movementSpeed * Time.deltaTime, 0));
+		}
+
+		if (Input.GetKey("a")) {
+			gameObject.transform.Translate( new Vector3(-1* movementSpeed * Time.deltaTime, 0));
+		}
+	}
+}
