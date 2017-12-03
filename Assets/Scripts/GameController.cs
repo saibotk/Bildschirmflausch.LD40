745b3d796331485f67c99f3bc2f0132eb9e34d82
Unknown <jan@k-oo.de>
2017-12-03 18:52:05 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+private List<string> jobTypes = new List<string>();
+
+// Use this for initialization
+void Start () {
+
+}
+
+// Update is called once per frame
+void Update () {
+	if (Time.realtimeSinceStartup >= lastJob + jobTimeInterval) {
+		addRandomJob ();
+	}
+}
+
+public void addRandomJob () {
+	string jobType = jobTypes[Random.Range (0, jobTypes.Count)];
+	switch (jobType) {
+	case "bring":
+		//      jobManager.AddJob (new Job ("test", "test", "super Test", 30));
+		break; 
+	}
+}
+}
