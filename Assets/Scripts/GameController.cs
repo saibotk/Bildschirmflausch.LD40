81cab0033d809e169659f23df33ee2ef2b50dd3c
Unknown <saibotk@users.noreply.github.com>
2017-12-03 18:52:30 +0100

    Partial Job System implementation

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private Jobmanager jobManager;
+	[SerializeField]
+	private int jobTimeInterval = 20;
+	private float lastJob = Time.realtimeSinceStartup;
+
+	private List<string> jobTypes = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		jobManager = new Jobmanager (this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.realtimeSinceStartup >= lastJob + jobTimeInterval) {
+			addRandomJob ();
+		}
+		jobManager.checkJobTimes ();
+	}
+
+	public void addRandomJob () {
+		string jobType = jobTypes[Random.Range (0, jobTypes.Count)];
+		switch (jobType) {
+		case "bring":
+			//			jobManager.AddJob (new Job ("test", "test", "super Test", 30));
+			break; 
+		}
+	}
+}
