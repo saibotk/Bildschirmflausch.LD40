f012fdc2539641c5ca2faa66314d2bbcb0fa35a6
Jan <Jidbo@users.noreply.github.com>
2017-12-03 18:19:41 +0100

    Added a new line

--- Assets/JobNPC.cs
+++ Assets/JobNPC.cs
@@ -8,6 +8,7 @@ public class JobNPC : MonoBehaviour, Interactable {
 	private Func<GameObject, bool> interactMethod = null;
 
 	public void Interact(GameObject player) {
+		
 		if (interactMethod != null) {
 			interactMethod (player);
 		}
