14901c0af2df9a9d216ede55bb8440876fda7941
Unknown <ldmusic@ld40.de>
2017-12-04 04:45:32 +0100

    Added Transistion of music

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioControl : MonoBehaviour
+{
+
+
+    public AudioMixerSnapshot maintheme;
+    public AudioMixerSnapshot lifttheme;
+    public AudioMixerSnapshot gameover;
+    public float bpm = 128;
+
+
+    private float m_TransitionIn;
+    private float m_TransitionOut;
+    private float m_QuarterNote;
+
+    // Use this for initialization
+    void Start() {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+        m_TransitionOut = 2;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+        if (col.CompareTag("Lift")) {
+            if (col.isTrigger) {
+                lifttheme.TransitionTo(m_TransitionIn);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.CompareTag("Lift")) {
+            if (col.isTrigger) {
+                maintheme.TransitionTo(m_TransitionOut);
+            }
+        }
+    }
+}
