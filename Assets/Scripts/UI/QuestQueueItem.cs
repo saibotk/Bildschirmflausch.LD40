301be955fd728cc3aba7ea906bee87233491d8ac
Unknown <saibotk@users.noreply.github.com>
2017-12-05 22:36:00 +0100

    Add timer to quests

--- Assets/Scripts/UI/QuestQueueItem.cs
+++ Assets/Scripts/UI/QuestQueueItem.cs
@@ -9,10 +9,40 @@ public class QuestQueueItem : MonoBehaviour {
 	private GameObject indicator;
 
 	[SerializeField]
+	private GameObject timer;
+
+	[SerializeField]
+	private Texture2D texTimer;
+
+	[SerializeField]
 	private GameObject icon;
 
 	// Use this for initialization
 	void Start () {
+
+	}
+
+	void Update() {
+		float timestate = (Time.realtimeSinceStartup - job.GetJobStartTime()) / job.GetJobTime() * 9;
+		if (timestate < 1) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(0, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else if (timestate < 2) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(16, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else if (timestate < 3) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(32, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else if (timestate < 4) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(48, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else if (timestate < 5) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(64, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else if (timestate < 6) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(80, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else if (timestate < 7) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(96, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else if (timestate < 8.5f) {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(112, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		} else {
+			timer.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texTimer, new Rect(128, 0, 15, 15), new Vector2(0.5f, 0.5f));
+		}
 
 	}
 
